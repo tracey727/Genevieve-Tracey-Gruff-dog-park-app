a93a98a0750d1d0595506d7929939d56c0976125
Tracey <positivity864@gmail.com>
2026-08-18 06:37:43 +1000

    Apply GENEVIEVE locked colour layer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './brand-colour-override.css';
 
 createRoot(document.getElementById('root')).render(<App />);
 
