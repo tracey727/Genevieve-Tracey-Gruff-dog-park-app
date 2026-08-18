865b905e222bc020091819db57d90355b432156d
Tracey <positivity864@gmail.com>
2026-08-19 09:12:43 +1000

    Connect verified Stripe monthly payment links

--- config.js
+++ config.js
@@ -2,9 +2,10 @@
 window.GENEVIEVE_CONFIG = Object.freeze({
   supportEmail: 'tracey@genevieveapp.com.au',
   website: 'https://genevieveapp.com.au',
+  stripeMode: 'live',
   paymentLinks: Object.freeze({
-    standardMonthly: '',
-    concessionMonthly: '',
+    standardMonthly: 'https://buy.stripe.com/3cI6oI3IadEnefWfQW1wY0h',
+    concessionMonthly: 'https://buy.stripe.com/5kQ7sMa6y7fZ4Fm48e1wY0d',
     standardAnnual: '',
     concessionAnnual: ''
   })
